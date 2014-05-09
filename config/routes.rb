8f48838f01b669e6e0785311f2623c2ab6cf6c11
syahmie <syahmie2008@gmail.com>
2014-05-09 17:29:09 +0800

    Sets app's root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hackthon::Application.routes.draw do
+  root 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
